c149ee75b77049b1b4e57f57b6ee39147cc09428
SummitKC <128647435+SummitKC@users.noreply.github.com>
2025-01-20 17:49:28 -0500

    Begining of PenModel + ViewModel

---
+++ TwoOkNotes/Model/PenModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Ink;
+using System.Windows.Media;
+
+namespace TwoOkNotes.Model
+{
+    public class PenModel
+    {
+        public Color PenColor { get; set; } 
+        public double Thickness { get; set; } 
+        public double Opacity { get; set; }
+        public StylusTip PenTip { get; set; }
+        public bool IsEraser { get; set; }
+
+        public PenModel(String color, double thickness, double opacity, StylusTip tip, bool isEraser)
+        {
+            PenColor = (Color)ColorConverter.ConvertFromString(color);
+            Thickness = thickness;
+            Opacity = opacity;
+            PenTip = tip;
+            IsEraser = isEraser;
+        }
+    }
+}
---
+++ TwoOkNotes/ViewModels/PenViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TwoOkNotes.Model;
+
+namespace TwoOkNotes.ViewModels
+{
+    class PenViewModel : EditingWIndowViewModel
+    {
+
+        public PenModel CurrentPen { get; set; }
+
+        public PenViewModel(PenModel pen)
+        {
+            CurrentPen = pen;
+        }
+
+    }
+}
