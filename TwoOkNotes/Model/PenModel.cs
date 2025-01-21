a6898e24c04d19023e2a3ae04c8cd85fa249b8f4
SummitKC <128647435+SummitKC@users.noreply.github.com>
2025-01-20 21:12:45 -0500

    Dynamic setting of Pen Object via PropertyChange in the model

--- TwoOkNotes/Model/PenModel.cs
+++ TwoOkNotes/Model/PenModel.cs
@@ -1,28 +1,72 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 using System.Windows.Ink;
 using System.Windows.Media;
+using TwoOkNotes.Util;
 
 namespace TwoOkNotes.Model
 {
-    public class PenModel
+    public class PenModel : ObservableObject
     {
-        public Color PenColor { get; set; } 
-        public double Thickness { get; set; } 
-        public double Opacity { get; set; }
-        public StylusTip PenTip { get; set; }
-        public bool IsEraser { get; set; }
+        private Color _penColor = Colors.White;
+        private double _thickness = 2.0;
+        private double _opacity = 1; 
+        private StylusTip _penTip = StylusTip.Ellipse;
+        private bool _isEraser = false; 
 
-        public PenModel(String color, double thickness, double opacity, StylusTip tip, bool isEraser)
+        public Color Color
         {
-            PenColor = (Color)ColorConverter.ConvertFromString(color);
-            Thickness = thickness;
-            Opacity = opacity;
-            PenTip = tip;
-            IsEraser = isEraser;
+            get => _penColor;
+            set
+            {
+                _penColor = value;
+                OnPropertyChanged(nameof(Color));
+            }
+        }
+
+        public double ThickNess
+        {
+            get => _thickness;
+            set
+            {
+                _thickness = value;
+                OnPropertyChanged(nameof(ThickNess));
+            }
+        }
+
+        public double Opacity
+        {
+            get => _opacity;
+            set
+            {
+                _opacity = value;
+                OnPropertyChanged(nameof(Opacity));
+            }
+        }
+
+        public StylusTip Tip
+        {
+            get => _penTip;
+            set
+            {
+                _penTip = value;
+                OnPropertyChanged(nameof(Tip));
+            }
+        }
+
+        public bool IsEraser
+        {
+            get => _isEraser;
+            set
+            {
+                _isEraser = value;
+                OnPropertyChanged(nameof(IsEraser));
+            }
         }
     }
 }
