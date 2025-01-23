bf0472ce6f9a805c4ce828b37f5c890efe66d424
SummitKC <128647435+SummitKC@users.noreply.github.com>
2025-01-23 15:04:52 -0500

    Switching PenModel logic to view model

--- TwoOkNotes/ViewModels/PenViewModel.cs
+++ TwoOkNotes/ViewModels/PenViewModel.cs
@@ -1,44 +1,41 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using System.Diagnostics;
-//using System.Linq;
-//using System.Text;
-//using System.Threading.Tasks;
-//using System.Windows.Ink;
-//using System.Windows.Input;
-//using System.Windows.Media;
-//using TwoOkNotes.Model;
-//using TwoOkNotes.Util;
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using TwoOkNotes.Model;
+using TwoOkNotes.Util;
 
-//namespace TwoOkNotes.ViewModels
-//{
-//    public class PenViewModel : EditingWIndowViewModel
-//    {
+namespace TwoOkNotes.ViewModels
+{
+    public class PenViewModel : ObservableObject
+    {
 
-//        public PenModel CurrentPenModel { get; set; }
-//        public ICommand ChangePenColorCommand { get; }
+        public readonly PenModel _penModel = new();
+        //public ICommand ChangePenColorCommand { get; }
 
-//        public PenViewModel()
-//        {
+        public PenViewModel(PenModel penModel)
+        {
 
-//            ChangePenColorCommand = new RelayCommand(ChangeColor);
-//        }
+            _penModel = penModel;
 
-//        private void ChangeColor(object? obj)
-//        {
-//            Debug.WriteLine(CurrentPenModel.GetType());
-//            if (CurrentPenModel != null)
-//            {
-//                if (obj is Color newColor)
-//                {
-//                    CurrentPenModel.PenColor = newColor;
-//                }
-//            }
-//            else
-//            {
-//                Debug.WriteLine("Not current pen");
-//            }
-//        }
-//    }
-//}
+        }
+
+        public Color PenColor
+        {
+            get => _penModel.PenColor;
+            set
+            {
+                _penModel.PenColor = value;
+                OnPropertyChanged(nameof(PenColor));
+            }
+
+        }
+      
+    }
+}
 
